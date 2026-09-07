2cbce4b16b307a5c9bb09bfb48119ff3643ab897
Gianluca Della Vedova <gianluca@dellavedova.org>
2026-09-07 18:18:22 +0200

    zs aliases

---
+++ aliases/zs.sh
@@ -0,0 +1,2 @@
+alias uzs='curl -fsSL https://github.com/gi-dellav/zerostack/releases/latest/download/zerostack-x86_64-unknown-linux-gnu.tar.gz | tar -xz -C ~/.local/bin'
+alias ums='curl -fsSL https://github.com/gi-dellav/multistack/releases/latest/download/multistack-x86_64-unknown-linux-gnu.tar.gz | tar -xz -C ~/.local/bin'
--- install.sh
+++ install.sh
@@ -1,8 +1,25 @@
-#!/usr/bin/bash 
+#!/usr/bin/env bash
+set -euo pipefail
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
-git clone https://github.com/gdv/bash.git ~/.bash
-cd ~/.bash
-cp bashrc ~/.bashrc
+# Install oh-my-bash, only if absent
+if [[ ! -f "$HOME/.oh-my-bash/oh-my-bash.sh" ]]; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
+fi
+
+# Clone this repo, idempotently
+if [[ ! -d "$HOME/.bash/.git" ]]; then
+    git clone --depth 1 https://github.com/gdv/bash.git "$HOME/.bash"
+fi
+
+# Point ~/.bashrc at the repo copy
+ln -sfn "$HOME/.bash/bashrc" "$HOME/.bashrc"
+
+# Expose aliases to oh-my-bash ($OSH/custom/*.sh are sourced at startup)
+mkdir -p "$HOME/.local/bin" "$HOME/.oh-my-bash/custom"
+shopt -s nullglob
+for p in "$HOME"/.bash/aliases/*.sh; do
+    ln -sfn "$p" "$HOME/.oh-my-bash/custom/"
+done
+
 
 
